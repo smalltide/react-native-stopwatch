562ca76d1b6d0da0e9c77df0a032889118f2d1b5
ice <smalltides@gmail.com>
2016-10-14 18:25:23 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # react-native-stopwatch
-stopwatch use react native
+use react native to create a stopwatch
+
+![alt text](https://github.com/smalltide/react-native-stopwatch/blob/master/screenshot.png "react-native-stopwatch")
+
+```
+	> git clone git@github.com:smalltide/react-native-stopwatch.git
+	> cd react-native-stopwatch
+	> react-native run-ios
+	> react-native run-android
+```
---
+++ index.ios.js
@@ -0,0 +1,164 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  TouchableHighlight,
+  Text,
+  View
+} from 'react-native';
+
+import formatTime from 'minutes-seconds-milliseconds';
+
+class StopWatch extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {timeElapsed: null, running: false, startTime: null, laps:[] };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={[styles.header]}>
+          <View style={[styles.timerWrapper]}>
+            <Text style={styles.timer}>
+              {formatTime(this.state.timeElapsed)}
+            </Text>
+          </View>
+          <View style={[styles.buttonWrapper]}>
+            {this.startStopButton()}
+            {this.lapButton()}
+          </View>
+        </View>
+
+        <View style={[styles.footer]}>
+          {this.laps()}
+        </View>
+      </View>
+    );
+  }
+
+  laps() {
+    return this.state.laps.map((time, index) => {
+      return (
+        <View key={index} style={styles.lap}>
+          <Text style={styles.lapText}>Lap #{index+1}</Text>
+          <Text style={styles.lapText}>{formatTime(time)}</Text>
+        </View>
+      );
+    });
+  }
+
+  startStopButton() {
+    const style = this.state.running ? styles.stopButton : styles.startButton ;
+
+    return (
+      <TouchableHighlight 
+        underlayColor='gray'
+        onPress={this.handleStartPress.bind(this)}
+        style={[styles.button, style]}
+      >
+        <Text>
+          {this.state.running ? 'Stop' : 'Start'}
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+  
+  lapButton() {
+    return (
+      <TouchableHighlight 
+        style={styles.button}
+        underlayColor='gray'
+        onPress={this.handleLapPress.bind(this)}
+      >
+        <Text>
+          Lap
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+
+  handleStartPress() {
+    if (this.state.running){
+      clearInterval(this.interval);
+      this.setState({running: false});
+      return;
+    }
+    
+    const startTime = new Date();
+    this.setState({startTime: new Date()});
+
+    this.interval = setInterval(() => {
+      this.setState({
+        timeElapsed: new Date() - this.state.startTime,
+        running: true
+      });
+    },30);
+  }
+
+  handleLapPress() {
+    const lap = this.state.timeElapsed;
+
+    this.setState({
+      startTime: new Date(),
+      laps: [...this.state.laps, lap]
+    });
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+  header: {
+    flex: 1
+  },
+  footer: {
+    flex: 1
+  },
+  timerWrapper: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: {
+    flex: 3,
+    flexDirection : 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  timer: {
+    fontSize: 60
+  },
+  button: {
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  startButton: {
+    borderColor: '#00CC00'
+  },
+  stopButton: {
+    borderColor: '#CC0000'
+  },
+  lap:{
+     justifyContent: 'space-around',
+     flexDirection: 'row'
+  },
+  lapText:{
+    fontSize: 30
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
